d7d6d5ebe737c487c7104c2c7a8a487a4bc5feaf
Bryan Quintero <bryanquinterog@gmail.com>
2022-06-30 15:22:57 -0500

    terminado ejercicio 10

--- Clase1 conociendo java/java project name clase01/ejercicios.java
+++ Clase1 conociendo java/java project name clase01/ejercicios.java
@@ -1,8 +1,12 @@
+import java.text.DecimalFormat;
 import java.util.Scanner;
+import java.util.concurrent.ThreadLocalRandom;
 
 public class ejercicios {
+    private static int numero;
+
     public static void main(String[] args) {
-        ejercicio7();
+        ejercicio10();
     }
     private static void ejercicio1() {
         var sc = new Scanner(System.in);
@@ -89,6 +93,67 @@ public class ejercicios {
     }
 
     private static void ejercicio7() {
+        var sc = new Scanner(System.in);
+        System.out.print("ingrese la primera nota: ");
+        var nota1 = sc.nextFloat();
+        System.out.print("ingrese la segunda nota: ");
+        var nota2 = sc.nextFloat();
+        System.out.print("ingrese la tercera nota: ");
+        var nota3 = sc.nextFloat();
+        sc.close();
+
+        var promedio = (nota1 + nota2 + nota3) / 3;
+        var resultado = (promedio >= 3.0)? "aprobado":"reprobado";
+        System.out.printf("el promedio del estudiante es %.2f por lo que el estudiante a %s", promedio, resultado);
+    }
+
+    private static void ejercicio8() {
+        var sc = new Scanner(System.in);
+        System.out.print("ingrese las horas trabajadas: ");
+        var horas = sc.nextInt();
+        sc.close();
+
+        DecimalFormat formatea = new DecimalFormat("###,###,###");
+        var valorhora = 30000;
+        var sueldo = horas * valorhora;
+
+        System.out.printf("el sueldo del trabajador es %s", formatea.format(sueldo));
+    }
+
+    private static void ejercicio9() {
+        var sc = new Scanner(System.in);
+        System.out.print("ingrese su numero: ");
+        var numero = sc.nextInt();
+        sc.close();
+
+        System.out.println("la tabla del " + numero);
+        for (int i = 0; i <= 10; i++) {
+            var tabla = numero + " * " + i + " = " + numero*i;
+            System.out.println(tabla);
+        }
+    }
+
+    private static void ejercicio10() {
+        var adivinar = ThreadLocalRandom.current().nextInt(1, 100);
+        try (var sc = new Scanner(System.in)) {
+            do {
+                System.out.print("ingrese su numero: ");
+                int numero = sc.nextInt();
+                System.out.print("ingrese su numero: ");
+
+                if (adivinar > numero) {
+                    System.out.println("el numero que busca es mayor");
+                } else if (adivinar < numero) {
+                    System.out.println("el numero que busca es menor");
+                } else if (adivinar == numero) {
+                    System.out.println("Has acertado, el número que pensé fue: " + adivinar);
+                    break;
+                }
+                } while (adivinar != numero);
+        }
+        }
+
+    private static void ejercicio11() {
         
     }
 }
